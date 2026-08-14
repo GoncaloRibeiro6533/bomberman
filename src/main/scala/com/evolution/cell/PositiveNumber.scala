6bd646e014753b2ed5284e6d36354f86bb453dc3
GoncaloRibeiro6533 <113711457+GoncaloRibeiro6533@users.noreply.github.com>
2026-08-14 15:44:28 +0100

    game loop simple version

--- src/main/scala/com/evolution/cell/PositiveNumber.scala
+++ src/main/scala/com/evolution/cell/PositiveNumber.scala
@@ -8,12 +8,4 @@ object PositiveNumber {
   def apply(value: Int): Option[PositiveNumber] = if (value >= 0) Some(new PositiveNumber(value))
   else None
 
-  trait PositiveNumberOp[T] {
-    def toPositiveNumber(value: T): Option[PositiveNumber]
-  }
-
-  implicit val positiveNumberOpInt: PositiveNumberOp[Int] = (value: Int) =>
-    if (value < 0) None
-    else
-      Some(new PositiveNumber(value))
 }
